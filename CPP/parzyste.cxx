6d28c74e12fba3df0f6c6949e651ae8ebac50122
aniajestem <alaskowska144@wp.pl>
2017-10-17 11:57:34 +0200

    parzysta

---
+++ CPP/parzyste.cxx
@@ -0,0 +1,13 @@
+/*
+ * parzyste.cxx
+.* program wpisuje wszystkie 2-cyfrowe lizby parzyste podzielne przez 3 w zakresie
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
--- CPP/petle_znak.cpp
+++ CPP/petle_znak.cpp
@@ -10,15 +10,32 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    char zn = '';
+    char zn = 't';
     
     
-    while (zn<
+    while (zn == 't' || zn == 'T' || zn == 'n' || zn == 'N' )
+    {
     cout << "Podaj znak: " << endl;
     cin >> zn;
     };
     
-    
+    //while (true)
+    //{
+        //cout << "Podaj znak: " ;
+        //cin >> zn
+        //switch (zn) 
+        //{
+            //case 't' :
+            //case 'T' :
+            //case 'n' :
+            //case "N' :
+                //cout << "Poprawny" ;
+                //break;
+                //default:
+                    //cout << "Inny";
+                    //break;
+        //}
+    //}
     
 	return 0;
 }

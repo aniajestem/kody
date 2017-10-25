971a12bcf1dd439fa3b736a7be6490cc8b31f1b0
aniajestem <alaskowska144@wp.pl>
2017-10-25 12:34:37 +0200

    tabele2

---
+++ CPP/ ćwiczenie.cpp
@@ -0,0 +1,25 @@
+/*
+ * zadanie.cpp
+ * program wypisuje wszystkie dwucyfrowe liczby parzyste podzielne prze 3 w zakresie <m;n> podanym przez użytkownika
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{ int m , n;
+    
+    while (m > 10 && m > n)
+    
+    {
+        cout <<"Podaj zakres: < m ; n > " << endl ;
+        cin >> m >> n ;
+        
+        if (m % 2 == 0 && )
+        
+            
+    }
+	
+	return 0;
+}
---
+++ CPP/tabele2.cpp
@@ -0,0 +1,51 @@
+/*
+ * tablice.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std; 
+
+void pobierzDane(int tab[], int ile) {
+    int i;
+    cout << "Podaj " << ile << " liczb: " << endl; 
+    for(i = 0; i < ile; i++) {
+        cin >> tab[i];
+    }
+}
+
+int sumuj(int tab[], int ile) {
+    int i;
+    int suma = 0;
+    cout << "Podane oceny: " << endl;
+        for(i = 0; i < ile; i++) {
+            cout << tab[i] << " ";
+            suma += tab[i];
+    }
+     return suma;
+}
+    float liczsrednia (int tab[], int ile){
+        return sumuj (tab, ile) / float (ile);
+}
+    
+    
+int main(int argc, char **argv)
+{
+    //const int ROZMIAR = 5;
+    int rozmiar = 0; // wartość może być zmieniana
+    cout << "Ile ocen: " << endl;
+    cin >> rozmiar;
+    
+	int liczby[rozmiar];
+
+    
+    pobierzDane(liczby, rozmiar);
+
+    
+    
+    
+    cout << "Suma ocen: " << sumuj(liczby, rozmiar) << endl;
+    cout << "Srednia: " << liczsrednia(liczby,rozmiar) << endl;
+	return 0;
+}

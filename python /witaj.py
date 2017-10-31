afabc847a0ea3720b067ec3f4aba112f58cd9255
aniajestem <alaskowska144@wp.pl>
2017-10-31 13:31:06 +0100

    python

---
+++ python /witaj.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+#  /3 oznacza znak nowej linii
+
+
+
+    #osoba = "Kuba Mazur"
+    #osoba = 'Maria Skłodowska KIRI'
+    osoba= input ('Jak się nazywasz?')
+    print ("witaj",osoba, "!" )
+    
+    rok = 2017
+    wiek = int(input ('Ile masz lat?'))
+    print ('Urodziłeś się w', rok-wiek, 'roku .')

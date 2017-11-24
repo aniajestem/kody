a40351963c1c9581ddb7c4f141f4d4dde791e167
aniajestem <alaskowska144@wp.pl>
2017-11-24 11:40:32 +0100

    py

--- python /minmax.py
+++ python /minmax.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 from random import randint
 
-def minimum (lista):
-    min = lista [0]
+
+def minmax(lista):
+    min = lista[0]
+    for el in enumerate(min):
+        if el < min:
+            min = el
+
+    return min
+
+
+
+
+
+def minimum(lista):
+    min = lista[0]
     for i in lista:
-        if  i < min:
+        if i < min:
             min = i
     return min
 
 
-def maksimum (lista):
-    max = lista [0]
+def maksimum(lista):
+    max = lista[0]
     for i in lista:
-        if  i > max:
+        if i > max:
             max = i
     return max
 
 
 def losuj(ile, zakres):
     lista = []
-    for i in range (ile):
+    for i in range(ile):
         lista.append(randint(0, zakres))
     return lista
 
@@ -30,9 +44,11 @@ def main(args):
     ile = 20
     zakres = 50
     lista = losuj(ile, zakres)
+    assert lmin([7, 4, 9, 1, 3, 0]) == 0
+    assert lmax([7, 4, 9, 1, 3, 0]) == 9
     print (lista)
-    print ("Minimum:", minimum(lista))
-    print ("Maksimum:", maksimum(lista))
+    print ("Min:", lmin(lista))
+    print ("max:", lmax(lista))
     return 0
 
 

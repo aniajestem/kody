198b04e62e4421c5523ed8cfe7ef0965536e1e0c
aniajestem <alaskowska144@wp.pl>
2017-11-28 13:30:40 +0100

    fabu

---
+++ python /fibonacci.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#elif = else + if
+
+def fib_iter (n):
+    """Funkcja wyświetla kolejmne wyrazt ciągu Fibonacciego
+    Funkcja zwraca N-ty wyraz ciągu
+    F(0)= 0
+    F(1)= 1
+    F(n)=F(n-1)+F(n+1) dla n>1 """
+
+    a,b=(0,1)
+    if n ==0:
+        print a
+        return a
+    elif n == 1:
+        print b
+        return b
+
+    for i in range (1, n):
+        pass
+
+    return b
+
+
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

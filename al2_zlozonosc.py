57fd9c56f146c1c77b891f0a0f562e845226ff8a
aniajestem <alaskowska144@wp.pl>
2018-02-27 13:22:56 +0100

    algorytmy

--- al2_zlozonosc.py
+++ al2_zlozonosc.py
@@ -6,20 +6,25 @@ def main(args):
     a=0
     while a < 1 or a > 99: # poprawny zakres a
         a = input ("podaj liczbe: ")
-        # for i in range(2,101,2):
-        #     if a == i:
-        #         print ("Liczba jest parzysta")
-        #         return 0
-        # print ("Liczba jest nieparzysta")
-        # return 0
-    i=2
-    while i<100:
-        if a == i:
-            print ("Parzysta")
-            return 0
-        i = i  + 2 #powieksza i o 2
-        print ("Nieparzysta")
-    return 0
+
+
+        for i in range(2,a+1,2):
+            print (i)
+            if a == i:
+                print ("Liczba jest parzysta")
+                return 0
+        print ("Liczba jest nieparzysta")
+        return 0
+
+
+    # i=2
+    # while i<100:
+    #     if a == i:
+    #         print ("Parzysta")
+    #         return 0
+    #     i = i  + 2 #powieksza i o 2
+    #     print ("Nieparzysta")
+    # return 0
 
 
 if __name__ == '__main__':

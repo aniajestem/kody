25b43980f2b108a7ae8eb70cf0f89e6a8f705d10
aniajestem <alaskowska144@wp.pl>
2019-01-07 12:52:15 +0100

    f

--- flask_czat/forms.py
+++ flask_czat/forms.py
@@ -2,27 +2,23 @@
 # -*- coding: utf-8 -*-
 #
 #  forms.py
-#  
-#  
-
 from flask_wtf import FlaskForm
 from wtforms import HiddenField, StringField, BooleanField
 from wtforms import SelectField, FormField, FieldList
 from wtforms.validators import Required
 
-blad1 = 'to pole jest wymagane'
+blad1 = 'To pole jest wymagane'
 
 class OdpForm(FlaskForm):
-    id = HiddenField('Odpowiedz id')
+    id = HiddenField('OdpowiedĹş id')
     pytanie = HiddenField('Pytanie id')
-    odpowiedz = StringField('Odpowiedz:',
-                            validators = [Required(message=blad1)])
+    odpowiedz = StringField('OdpowiedĹş:',
+                             validators=[Required(message=blad1)])
     odpok = BooleanField('Poprawna:')
-    
+
 class DodajForm(FlaskForm):
     id = HiddenField('Pytanie id')
-    pytanie = StringField('Treść pytania: ',
-                            validators = [Required(message=blad1)])
+    pytanie = StringField('TreĹÄ pytania:',
+                          validators=[Required(message=blad1)])
     kategoria = SelectField('Kategoria', coerce=int)
     odpowiedzi = FieldList(FormField(OdpForm), min_entries=3)
-    
